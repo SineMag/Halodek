430b9caa019eac3c62599bde9803d13a7461e2a2
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2026-01-08 09:51:02 +0200

    feat: added the profile image with transparernt background

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,14 +6,14 @@ import ChatListItem from '@/components/ui/ChatListItem';
 import FloatingActionButton from '@/components/ui/FloatingActionButton';
 
 const chatData = [
-  { id: '1', name: 'Faza Dzikrulloh', message: 'Typing...', time: '14:30', image: require('@/assets/images/Faza Dzikrulloh.png') },
+  { id: '1', name: 'Faza Dzikrulloh', message: 'Typing...', time: '14:30', image: require('@/assets/images/Faza.png') },
   { id: '2', name: 'Hatypo Studio', message: 'Faza Can you help me to do with new project...', time: '14:30', unreadCount: 2, image: require('@/assets/images/Hatypo Studio.png') },
-  { id: '3', name: 'Zhofran A', message: 'I think we should upgrade the social media...', time: '14:00', unreadCount: 2, image: require('@/assets/images/Zhofran A.png') },
-  { id: '4', name: 'Vito Arvy', message: 'Okay adhit, I\'ll tel faza about it 👍🏼 ', time: '13:40', image: require('@/assets/images/Vito Arvy.png') },
+  { id: '3', name: 'Zhofran A', message: 'I think we should upgrade the social media...', time: '14:00', unreadCount: 2, image: require('@/assets/images/Zhofran.png') },
+  { id: '4', name: 'Vito Arvy', message: 'Okay adhit, I\'ll tel faza about it 👍🏼 ', time: '13:40', image: require('@/assets/images/Vito.png') },
   { id: '5', name: 'Raul', message: 'Thanks Raul! 🙏🏼✨', time: 'Yesterday', sent: true, image: require('@/assets/images/Raul.png') },
-  { id: '6', name: 'Farhan Bagas', message: 'Great work farhan! 👍🏼 I\'ll tell faza about logo...', time: '29/08/22', sent: true, image: require('@/assets/images/Farhan Bagas.png') },
+  { id: '6', name: 'Farhan Bagas', message: 'Great work farhan! 👍🏼 I\'ll tell faza about logo...', time: '29/08/22', sent: true, image: require('@/assets/images/Farhan.png') },
   { id: '7', name: 'Abdull', message: 'Perfecto!🔥 I will check it later', time: '29/08/22', sent: true, image: require('@/assets/images/Abdull.png') },
-  { id: '8', name: 'Ibe hatypo', message: 'nicely done bro! 🤙🏼', time: '29/08/22', sent: true, image: require('@/assets/images/Ibe hatypo.png')  },
+  { id: '8', name: 'Ibe hatypo', message: 'nicely done bro! 🤙🏼', time: '29/08/22', sent: true, image: require('@/assets/images/Ibe.png')  },
 ];
 
 export default function Home() {
